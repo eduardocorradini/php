110a72acfbaca32a612c921c72a9fbc3f0a77baa
Eduardo Corradini <eduardo_corradini@yahoo.com.br>
2020-05-24 12:21:22 -0300

    resolvi o problema

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php
 
-<<<<<<< HEAD
+
 echo "Olá Mundo 456!";
 
 echo "olá Udemy!";
 
 
-=======
-echo "Olá Mundo 123!";
 
-echo "Olá Hcode";
->>>>>>> c6623c181266a41763b2fc1d55e08a4245a0bb1c
 
 ?>
